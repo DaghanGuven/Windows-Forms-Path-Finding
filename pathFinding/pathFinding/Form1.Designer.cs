1a17d26b1d0ea5736e0db8184250a3a087db5649
Dağhan Güven <moderator0180@gmail.com>
2024-09-02 18:38:39 +0300

    Add files via upload

---
+++ pathFinding/pathFinding/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace pathFinding
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioWalls = new System.Windows.Forms.RadioButton();
+            this.radioStart = new System.Windows.Forms.RadioButton();
+            this.radioEnd = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioWalls
+            // 
+            this.radioWalls.AutoSize = true;
+            this.radioWalls.Checked = true;
+            this.radioWalls.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioWalls.ForeColor = System.Drawing.Color.White;
+            this.radioWalls.Location = new System.Drawing.Point(12, 185);
+            this.radioWalls.Name = "radioWalls";
+            this.radioWalls.Size = new System.Drawing.Size(102, 18);
+            this.radioWalls.TabIndex = 1;
+            this.radioWalls.TabStop = true;
+            this.radioWalls.Text = "Place Walls";
+            this.radioWalls.UseVisualStyleBackColor = true;
+            // 
+            // radioStart
+            // 
+            this.radioStart.AutoSize = true;
+            this.radioStart.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioStart.ForeColor = System.Drawing.Color.White;
+            this.radioStart.Location = new System.Drawing.Point(12, 209);
+            this.radioStart.Name = "radioStart";
+            this.radioStart.Size = new System.Drawing.Size(165, 18);
+            this.radioStart.TabIndex = 1;
+            this.radioStart.Text = "Place Starting Point";
+            this.radioStart.UseVisualStyleBackColor = true;
+            // 
+            // radioEnd
+            // 
+            this.radioEnd.AutoSize = true;
+            this.radioEnd.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioEnd.ForeColor = System.Drawing.Color.White;
+            this.radioEnd.Location = new System.Drawing.Point(12, 233);
+            this.radioEnd.Name = "radioEnd";
+            this.radioEnd.Size = new System.Drawing.Size(151, 18);
+            this.radioEnd.TabIndex = 1;
+            this.radioEnd.Text = "Place Ending Point";
+            this.radioEnd.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 257);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(183, 71);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Find The Path";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.White;
+            this.richTextBox1.Location = new System.Drawing.Point(201, 186);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.richTextBox1.Size = new System.Drawing.Size(312, 92);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.TabStop = false;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(201, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Randomize";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Consolas", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(362, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(151, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(525, 338);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioEnd);
+            this.Controls.Add(this.radioStart);
+            this.Controls.Add(this.radioWalls);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Path Finding";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.RadioButton radioWalls;
+        private System.Windows.Forms.RadioButton radioStart;
+        private System.Windows.Forms.RadioButton radioEnd;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ pathFinding/pathFinding/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace pathFinding
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool startingPointExists = false;
+        bool endingPointExists = false;
+        bool holdingLC = false;
+
+        public void PictureBoxClicked(object sender, EventArgs e)
+        {
+            // The picturebox that was clicked
+            PictureBox pB = sender as PictureBox;
+
+            // Check if walls should be placed, then place the wall if so.
+            if (radioWalls.Checked)
+            {
+                pB.BackColor = radioWalls.Checked && pB.BackColor == Color.White ? Color.BlueViolet : Color.White;
+                return;
+            }
+            
+            // Check if starting point should be placed, remove the other starting point (if exists) and add another point if so.
+            if (radioStart.Checked)
+            {
+                foreach (Control control in Controls)
+                {
+                    if (control.GetType() == typeof(PictureBox) && control.BackColor == Color.ForestGreen) 
+                    {
+                        control.BackColor = Color.White;
+                        break;
+                    }
+                }
+                pB.BackColor = Color.ForestGreen;
+                return;
+            }
+
+            // Check if ending point should be placed, remove the other starting point (if exists) and add another point if so.
+            if (radioEnd.Checked)
+            {
+                foreach (Control control in Controls)
+                {
+                    if (control.GetType() == typeof(PictureBox) && control.BackColor == Color.Red)
+                    {
+                        control.BackColor = Color.White;
+                        break;
+                    }
+                }
+                pB.BackColor = Color.Red;
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int locX = 12;
+            int locY = 8;
+
+            for (int i = 1; i <= 192; i++)
+            {
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Location = new Point(locX, locY);
+                pictureBox.BackColor= Color.White;
+                pictureBox.Click += PictureBoxClicked;
+                pictureBox.Size = new Size(20,20);
+                pictureBox.Name = "p" + i;
+                locX += 21;
+                if (i % 24 == 0)
+                {
+                    locX = 12;
+                    locY += 22;
+                }
+                Controls.Add(pictureBox);
+            }
+        }
+
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox1.Text += richTextBox1.Text==""?"":"\n";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            foreach (Control control in Controls)
+            {
+                if (control.BackColor == Color.Brown)
+                {
+                    control.BackColor = Color.White;
+                }
+            }
+
+            if (!FindStartAndEnd(out PictureBox start, out PictureBox end))
+            {
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text = "Error: Choose a valid starting and ending point.";
+                return;
+            }
+
+            Queue<PictureBox> queue = new Queue<PictureBox>();
+            Dictionary<PictureBox, PictureBox> cameFrom = new Dictionary<PictureBox, PictureBox>();
+
+            queue.Enqueue(start);
+            cameFrom[start] = null;
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                if (current == end)
+                {
+                    MarkPath(cameFrom, end);
+                    end.BackColor = Color.Red;
+                    richTextBox1.ForeColor = Color.Green;
+                    richTextBox1.Text = "The ending point was found successfully!";
+                    return;
+                }
+
+                foreach (var neighbor in GetNeighbouringBoxes(current))
+                {
+                    if (neighbor != null && neighbor.BackColor != Color.BlueViolet && !cameFrom.ContainsKey(neighbor))
+                    {
+                        queue.Enqueue(neighbor);
+                        cameFrom[neighbor] = current;
+                    }
+                }
+            }
+
+            richTextBox1.ForeColor = Color.Red;
+            richTextBox1.Text = "No path found.";
+        }
+
+        private void MarkPath(Dictionary<PictureBox, PictureBox> cameFrom, PictureBox end)
+        {
+            var current = end;
+            while (cameFrom[current] != null)
+            {
+                current.BackColor = Color.Brown;
+                current = cameFrom[current];
+            }
+        }
+
+        private bool FindStartAndEnd(out PictureBox start, out PictureBox end)
+        {
+            start = null;
+            end = null;
+            foreach (Control control in Controls)
+            {
+                if (control.GetType() == typeof(PictureBox))
+                {
+                    if (control.BackColor == Color.ForestGreen) start = (PictureBox)control;
+                    if (control.BackColor == Color.Red) end = (PictureBox)control;
+                }
+            }
+            return start != null && end != null;
+        }
+
+
+        // [0]: Right, [1]: Up, [2]: Left, [3]: Down
+        List<PictureBox> GetNeighbouringBoxes(PictureBox pictureBox)
+        {
+            List<PictureBox> NeighbouringBoxes= new List<PictureBox>();
+            int pB_Loc = Convert.ToInt32(pictureBox.Name.Remove(0, 1));
+
+            if (pB_Loc%24== 0)
+            {
+                NeighbouringBoxes.Add(null);
+            }
+            else
+            {
+                foreach (Control control in Controls)
+                {
+                    if(control.GetType() == typeof(PictureBox))
+                    {
+                        if (control.Name == "p" + (pB_Loc + 1))
+                        {
+                            NeighbouringBoxes.Add(control as PictureBox);
+                            break;
+                        }
+                    } 
+                }
+            }
+            if(NeighbouringBoxes.Count == 0) {
+                richTextBox1.Clear();
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text += "Error: Couldn't find the box on the right.";
+                return NeighbouringBoxes;
+            }
+
+
+
+            if (pB_Loc<25)
+            {
+                NeighbouringBoxes.Add(null);
+            }
+            else
+            {
+                foreach (Control control in Controls)
+                {
+                    if (control.GetType() == typeof(PictureBox))
+                    {
+                        if (control.Name == "p" + (pB_Loc - 24))
+                        {
+                            NeighbouringBoxes.Add(control as PictureBox);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (NeighbouringBoxes.Count == 1)
+            {
+                richTextBox1.Clear();
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text += "Error: Couldn't find the upper box.";
+                return NeighbouringBoxes;
+            }
+
+
+
+            if (pB_Loc % 24 == 1)
+            {
+                NeighbouringBoxes.Add(null);
+            }
+            else
+            {
+                foreach (Control control in Controls)
+                {
+                    if (control.GetType() == typeof(PictureBox))
+                    {
+                        if (control.Name == "p" + (pB_Loc - 1))
+                        {
+                            NeighbouringBoxes.Add(control as PictureBox);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (NeighbouringBoxes.Count == 2)
+            {
+                richTextBox1.Clear();
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text += "Error: Couldn't find the box on the left.";
+                return NeighbouringBoxes;
+            }
+
+
+
+            if (pB_Loc <= 192 && pB_Loc>168)
+            {
+                NeighbouringBoxes.Add(null);
+            }
+            else
+            {
+                foreach (Control control in Controls)
+                {
+                    if (control.GetType() == typeof(PictureBox))
+                    {
+                        if (control.Name == "p" + (pB_Loc + 24))
+                        {
+                            NeighbouringBoxes.Add(control as PictureBox);
+                            break;
+                        }
+                    }
+                }
+            }
+            if (NeighbouringBoxes.Count == 3)
+            {
+                richTextBox1.Clear();
+                richTextBox1.ForeColor = Color.Red;
+                richTextBox1.Text += "Error: Couldn't find the down box.";
+                return NeighbouringBoxes;
+            }
+
+            return NeighbouringBoxes;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text= "";
+            foreach (Control control in Controls)
+            {
+                control.BackColor = (control.BackColor == Color.Red || control.BackColor == Color.ForestGreen || control.BackColor == Color.Brown|| control.BackColor ==Color.BlueViolet) ? Color.White:control.BackColor;
+            }
+        }
+
+        private void button2_Click_1(object sender, EventArgs e) //randomize
+        {
+        startPoint:
+            bool sRed = false;
+            bool sGreen = false;
+            button2_Click(sender, e);
+            Random rand = new Random();
+            foreach (Control control in Controls)
+            {
+                if(control.GetType() == typeof(PictureBox))
+                {
+                    if (rand.Next(1, 11) < 3) control.BackColor = Color.BlueViolet;
+                    if(rand.Next(1,101)<5&&!sGreen){ control.BackColor = Color.ForestGreen; sGreen = true; }
+                    if (rand.Next(1, 101) < 5&&!sRed) { control.BackColor = Color.Red;sRed = true; }
+                }
+            }
+
+
+
+            if (!FindStartAndEnd(out PictureBox start, out PictureBox end))
+            {
+                goto startPoint;
+            }
+
+            Queue<PictureBox> queue = new Queue<PictureBox>();
+            Dictionary<PictureBox, PictureBox> cameFrom = new Dictionary<PictureBox, PictureBox>();
+
+            queue.Enqueue(start);
+            cameFrom[start] = null;
+            int i = 0;
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                if (current == end)
+                {
+                    // maze possible
+                    return;
+                }
+
+                foreach (var neighbor in GetNeighbouringBoxes(current))
+                {
+                    if (neighbor != null && neighbor.BackColor != Color.BlueViolet && !cameFrom.ContainsKey(neighbor))
+                    {
+                        queue.Enqueue(neighbor);
+                        cameFrom[neighbor] = current;
+                        
+                    }
+                }
+                
+            }
+            goto startPoint;
+        }
+    }
+}
